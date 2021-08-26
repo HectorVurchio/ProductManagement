6da4ca15f65eba4b0942f18d58c1d7dc00e0ade4
Hector Vurchio <hectorvurchio@gmail.com>
2021-08-26 14:51:02 -0400

    Practice 6 1 Create Food and Drink Classes That Extends Product

---
+++ src/labs/pm/data/Drink.java
@@ -0,0 +1,22 @@
+/**
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the license, or 
+* (at your option) later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARANTY, witout even the implied waranty at
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. see the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+package labs.pm.data;
+import java.math.BigDecimal;
+public class Drink extends Product{
+	public Drink(int id,String name,BigDecimal price,Rating rating){
+		super(id,name,price,rating);
+	}
+}
---
+++ src/labs/pm/data/Food.java
@@ -0,0 +1,34 @@
+/**
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the license, or 
+* (at your option) later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARANTY, witout even the implied waranty at
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. see the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+package labs.pm.data;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+
+public class Food extends Product{
+	private LocalDate bestBefore;
+	
+	public Food(int id,String name,BigDecimal price,Rating rating,LocalDate bestBefore){
+		super(id,name,price,rating);
+		this.bestBefore = bestBefore;
+	}
+	/**
+	* Get the value of bestBefore
+	* @return the value of bestBefore
+	*/
+	public LocalDate getBestBefore(){
+		return this.bestBefore;
+	}
+}
